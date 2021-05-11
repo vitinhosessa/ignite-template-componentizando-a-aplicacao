4d23bec5d176fabf2157b5d6b9a7ee9c486c96fe
Vitinho <vitinho.dev@gmail.com>
2021-05-11 18:05:46 -0300

    desafio finalizado :)

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from '../App';
+import { MovieCard } from './MovieCard'
+import '../styles/content.scss';
+
+interface IContentProps {
+  selectedGenre: {
+    title: string;
+  }
+  movies: MovieProps[]
+}
+
+export function Content(props: IContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from './Button';
+
+interface ISideBarProps {
+  parentCallback(id: number): void
+  genres: GenreResponseProps[]
+  selectedGenreId: number
+}
+
+export function SideBar(props: ISideBarProps) {
+
+  function sendDataToParent(id: number) {
+    props.parentCallback(id)
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => sendDataToParent(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+    </nav>
+  )
 }
